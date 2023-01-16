f7afc045c60d37b20b6f8293c34f5aa2b23755e5
Raj-Srivastava <77150093+Raj-Srivastava@users.noreply.github.com>
2023-01-16 22:08:30 +0530

    Add files via upload

---
+++ 1.py
@@ -0,0 +1,11 @@
+# This program for taking 5 subject marks and calculate total and average
+print("You have to enter marks out of 100")
+s1=int(input("Enter First Subject Marks : "))
+s2=int(input("Enter Second Subject Marks : "))
+s3=int(input("Enter Third Subject Marks : "))
+s4=int(input("Enter Fourth Subject Marks : "))
+s5=int(input("Enter Fifth Subject Marks : "))
+total_mks=s1+s2+s3+s4+s5
+average_mks=total_mks/5
+print("Total Marks Obtained = ",total_mks)
+print("Average Marks = ",average_mks)
