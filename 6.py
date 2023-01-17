eeedb4ac71d9b1249b5d6987d5c2898ecfb89b92
Raj-Srivastava <77150093+Raj-Srivastava@users.noreply.github.com>
2023-01-17 21:07:36 +0530

    Add files via upload

---
+++ 6.py
@@ -0,0 +1,8 @@
+#This Program to print number of years , weeks, days in entered number of days-
+days=int(input("Enter Number of Days : "))
+years=days//365
+months=days//30
+weeks=days//7
+print("Number of years : ",years)
+print("Number of months : ",months)
+print("Number of weeks : ",weeks)
