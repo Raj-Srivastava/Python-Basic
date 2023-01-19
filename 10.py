46eb01c9322f50bf045aa85d2fa94a79e176f2ce
Raj-Srivastava <77150093+Raj-Srivastava@users.noreply.github.com>
2023-01-19 23:50:11 +0530

    Add files via upload

---
+++ 10.py
@@ -0,0 +1,6 @@
+#This Program for checking whether entered number is even or not
+num=int(input("Enter a number :"))
+if (num%2)==0:
+ print("Entered Number is Even ")
+else:
+ print("Entered Number is odd ")
