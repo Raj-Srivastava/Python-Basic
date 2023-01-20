d35ed404b29eeef760b0d39311b115d94bf71aa1
Raj-Srivastava <77150093+Raj-Srivastava@users.noreply.github.com>
2023-01-20 18:07:45 +0530

    Add files via upload

---
+++ 14.py
@@ -0,0 +1,6 @@
+#This program to check whether entered year is leap or not
+year=int(input("Enter year :"))
+if (year%4)==0:
+    print("Entered year is leap year")
+else:
+    print("Entered year is not a leap year")
